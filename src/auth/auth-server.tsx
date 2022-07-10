5d847298132acb21e0ec7671e741b1d5dfac3040
Aleksandr Sugak <aleksandr.sugak@gmail.com>
2022-07-10 22:18:50 +0200

    auth flow todo

--- src/auth/auth-server.tsx
+++ src/auth/auth-server.tsx
@@ -10,6 +10,26 @@ import { LoginView, AllActions, initialState } from './auth-view'
 // TODO: set password for new users
 // const salt = crypto.randomBytes(16).toString('hex')
 
+
+/* 
+TODO: auth flow:
+- send auth token in login response
+  - {token: <payload>.<signature>, expires_in: N}
+    where payload: { userId, userRole, iat (Issued At Time), etc. }
+    and signature is encrypted (sha512?) payload
+  - read secret from env var on server
+- on client, store token (cookie or local storage)
+- on client, decode payload and store in state
+  - https://stackoverflow.com/a/38552302
+- on client, send token with every api request as 'Authorization: Bearer <token>'
+- on server, verify token for protected routes (signature = crypto(payload, secret))
+  if token is invalid:
+  - if calling server rendering, redirect to /login 
+  - if calling api, return 403
+- on client, if got 403, redirect to /login and clear token
+*/
+
+
 function passwordHash(password: string, salt: string): string {
   return crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex')
 }
@@ -58,6 +78,7 @@ export const init: EndpointInit = (dependencies: Dependencies, app: express.Expr
     const reqPasswordHash = passwordHash(request.password, salt)
 
     if (reqPasswordHash === dbPasswordHash) {
+      // TODO: generate token
       return res.json({msg: 'logged in'} as LoginResponse)
     }
 
