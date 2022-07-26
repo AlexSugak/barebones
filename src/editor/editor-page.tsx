dea33e484f79dbd67391e0bd54ec514d951f0a18
Aleksandr Sugak <aleksandr.sugak@gmail.com>
2022-07-26 23:33:03 +0200

    editor time travel working

--- src/editor/editor-page.tsx
+++ src/editor/editor-page.tsx
@@ -1,19 +1,20 @@
 import { React } from '../react'
 import * as monaco from '../monaco'
 import { Disposable } from '../disposable'
-import { filter, map, tap, Observable, Subject } from "../rx"
+import * as Rx from "../rx"
 import { useDisposable, useSubscription } from '../hooks'
+import { invariant } from '../errors'
 
 // TODO: do not hardcode localhost
 const editorWs = 'ws://localhost:3000/editor/ws'
 
 interface EditorWS extends Disposable{
-  messages: Observable<string>
+  messages: Rx.Observable<string>
   send(msg: string): void
 }
 
 const getEditorWS = (): EditorWS => {
-  const serverMessages = new Subject<string>()
+  const serverMessages = new Rx.Subject<string>()
 
   const ws = new WebSocket(editorWs)
 
@@ -41,6 +42,12 @@ const getEditorWS = (): EditorWS => {
 }
 
 
+type Change = monaco.editor.IModelContentChange
+type ChangesWithUndo = { changes: Change[], invertedChanges: Change[] } 
+type EditorChange = { changes: Change[], prevContent: string }
+
+const defaultCode = 'function hello() {}'
+
 export const Editor = ({}) => {
   const editorElementRef = React.useRef<HTMLDivElement | undefined>(undefined)
 
@@ -51,7 +58,7 @@ export const Editor = ({}) => {
     document.body.appendChild(loaderScript)
 
     const editorSettings: monaco.editor.IStandaloneEditorConstructionOptions = {
-      value: 'function hello() {}',
+      value: '',
       language: 'javascript',
       minimap: {
         enabled: false
@@ -79,36 +86,223 @@ export const Editor = ({}) => {
     }
   }, [])
 
+  const changesObs = new Rx.Subject<EditorChange>()
+  const editorRef = React.useRef<monaco.editor.IStandaloneCodeEditor>()
+  const isTimeTraveling = React.useRef(false)
   const initEditor = (editor: monaco.editor.IStandaloneCodeEditor) => {
-    editor.getModel().pushEditOperations(
-      [], 
-      [{
-        forceMoveMarkers: true,
-        range: {
-            startLineNumber: 1,
-            startColumn: 1,
-            endLineNumber: 1,
-            endColumn: 1
-        },
-        text: 'const a = "b" \n'
-      }],
-      null
-    )
+    editorRef.current = editor
+    const model = editor.getModel()
+    let content = model.getValue()
+
+    model.onDidChangeContent(e => {
+      if(!isTimeTraveling.current){
+        changesObs.next({changes: e.changes, prevContent: content})
+      }
+      content = model.getValue()
+    })
+
+    requestAnimationFrame(() => 
+      model.pushEditOperations(
+        [], 
+        [{
+          forceMoveMarkers: true,
+          range: {
+              startLineNumber: 1,
+              startColumn: 1,
+              endLineNumber: 1,
+              endColumn: 1
+          },
+          text: defaultCode
+        }],
+        null
+      ))
   }
 
   React.useEffect(() => {
     (window as any).initEditor = initEditor
   }, [])
 
+  //start with an empty change to be able to rewind all the way to the empty state
+  const changesWithUndo = React.useRef<ChangesWithUndo[]>([{changes: [], invertedChanges: []}])
+
+  useSubscription(changesObs.pipe(
+    Rx.map(c => ({changes: c.changes, invertedChanges: invertChanges(c.changes, c.prevContent)})),
+    Rx.tap(c => console.log("change", c)),
+    Rx.tap(c => {
+      changesWithUndo.current.push(c)
+      prevPosition.current = changesWithUndo.current.length - 1
+    })
+  ))
+
+  const prevPosition = React.useRef(0)
+  const timePosition = new Rx.Subject<number>()
+  useSubscription(timePosition.pipe(
+    // Rx.tap(p => console.log(p))
+    Rx.tap(p => {
+      timeTravel(p)
+    })
+  ))
+
+  const timeTravel = (timePosition: number) => {
+    // timePosition is from 0 to 100
+    // prevPosition is index in changesArray
+
+    isTimeTraveling.current = true
+
+    const changesTotal = changesWithUndo.current.length
+    const prev = prevPosition.current
+    const scale = 100 / changesTotal
+    const timeScaled = Math.floor(timePosition / scale)
+    
+    console.log({changesTotal, prev, timePosition, timeScaled, scale})
+
+    if (timeScaled === prev) {
+      return
+    }
+
+    if (timeScaled > prev) { // redo
+      const delta = [...changesWithUndo.current].splice(prev + 1, timeScaled - prev)
+      console.log(delta)
+      redo(delta, () => {
+        isTimeTraveling.current = false
+        prevPosition.current = timeScaled
+      })
+    } else { // undo
+      const delta = [...changesWithUndo.current].splice(timeScaled + 1, prev - timeScaled)
+      console.log(delta)
+      undo(delta, () => {
+        isTimeTraveling.current = false
+        prevPosition.current = timeScaled
+      })
+    }
+  }
+
+  const redo = (c: ChangesWithUndo[], onFinished: () => void, delay = 0) => {
+    if (c.length === 0) {
+      onFinished()
+      return
+    }
+
+    const first = c.splice(0, 1)[0]
+
+    editorRef.current.getModel().pushEditOperations(
+      [], 
+      first.changes.map(c => ({...c, forceMoveMarkers: true})), 
+      null
+    )
+    if (delay > 0) {
+      setTimeout(() => redo(c, onFinished, delay), delay)
+    } else {
+      redo(c, onFinished)
+    }
+  }
+
+  const undo = (c: ChangesWithUndo[], onFinished: () => void, delay = 0) => {
+    if (c.length === 0) {
+      onFinished()
+      return
+    }
+
+    const last = c.pop()
+
+    editorRef.current.getModel().pushEditOperations(
+      [], 
+      last.invertedChanges.map(c => ({...c, forceMoveMarkers: true})), 
+      null
+    )
+    if (delay > 0) {
+      setTimeout(() => undo(c, onFinished, delay), delay)
+    } else {
+      undo(c, onFinished)
+    }
+  }
+
+  const undoAll = () => {
+    isTimeTraveling.current = true
+    undo(changesWithUndo.current, () => {isTimeTraveling.current = false}, 500)
+  }
+
+  
+
   const editorWS = useDisposable(getEditorWS())
   // TODO: handle messages from server
   // useSubscription(editorWS.messages)
 
   return (
-    <div 
-      id="editorContainer" 
-      ref={editorElementRef} 
-      style={{width: '800px', height: '600px', border: '1px solid grey'}}>
+    <div>
+      <div 
+        id="editorContainer" 
+        ref={editorElementRef} 
+        style={{width: '400px', height: '200px', margin: '10px', border: '1px solid grey'}}>
+      </div>
+      {/* <div style={{margin: '10px'}}>
+        <button className="btnPrimary" onClick={undoAll}>Undo all</button>
+      </div> */}
+      <div style={{margin: '10px', width: '400px'}}>
+        <TimeRange positionListener={timePosition} />
+      </div>
     </div>
   )
+}
+
+const TimeRange = ({positionListener}: {positionListener: Rx.Observer<number>}) => {
+  const [timeSliderValue, setTimeSliderValue] = React.useState(100)
+
+  const updatePosition = (newPosition: number) => {
+    setTimeSliderValue(newPosition)
+    positionListener.next(newPosition)
+  }
+
+  return (<input 
+    type="range"
+    min="0" 
+    max="100" 
+    value={timeSliderValue}
+    onChange={e => updatePosition(Number(e.target.value))}
+    style={{width: '100%', cursor: 'pointer'}} 
+  />)
+}
+
+// TODO: move this to the server to not add load to the client ?
+
+function invertChanges(changes: Change[], prevContent: string): Change[] {
+  invariant(changes.length === 1, 'inverting array of changes is not implemented') 
+  // TODO: support multiple changes
+
+  const [change, ...rest] = changes
+
+  if (change.rangeLength === 0) { // insert
+    const newLineInserts = (change.text.match(/\n/g) || []).length
+    const endsWithNewLine = change.text.endsWith("\n")
+    return [
+      {
+        ...change,
+        range: {
+          ...change.range,
+          endColumn: endsWithNewLine ? 1 : change.range.endColumn + change.text.length,
+          endLineNumber: change.range.endLineNumber + newLineInserts
+        },
+        rangeLength: change.text.length,
+        text: ''
+      }
+    ]
+  } else { // delete
+    // split text into lines but leave the \n char
+    const lines = prevContent.match(/.*?\n|.+$/g)
+    // TODO: handle multi-line deletes
+    const deletedAtLine = lines[change.range.startLineNumber - 1]
+    const deletedText = deletedAtLine.substring(change.range.startColumn - 1, change.range.endColumn - 1)
+
+    return [
+      {
+        ...change,
+        range: {
+          ...change.range,
+          endColumn: change.range.startColumn
+        },
+        rangeLength: 0,
+        text: deletedText
+      }
+    ]
+  }
 }
--- src/errors.ts
+++ src/errors.ts
@@ -29,3 +29,39 @@ export class MatchingNotExhaustiveError extends CustomError {
 export function assertNever(x: never): never {
   throw new MatchingNotExhaustiveError(x)
 }
+
+export class InvariantError extends CustomError {
+  constructor(msg?: string, public input = 'empty') {
+    super('Invariant condition failed: ' + (Boolean(msg) ? msg : '(unnamed)'))
+  }
+
+  toString() {
+    return `${this.message} on input[${this.input}]`
+  }
+}
+
+export function invariant(cond: boolean): asserts cond
+export function invariant(cond: boolean, msg: string): asserts cond
+export function invariant(cond: boolean, msg: string, input: string): asserts cond
+export function invariant(cond: boolean, msg: string, input: () => string): asserts cond
+export function invariant(cond: boolean, getMsg: () => string): asserts cond
+export function invariant(
+  cond: boolean,
+  msg?: (() => string) | string,
+  input?: (() => string) | string
+): asserts cond {
+  if (!cond) {
+    const err = new InvariantError(
+      typeof msg === 'string' ? msg : msg && msg(),
+      input !== undefined && typeof input !== 'string' ? input() : input as string
+    )
+    // drop any signs to invariant closure to get a more clear stack trace in invariant errors
+    // it works only in dev env, but it provides much better DX in debugging jest tests
+    err.stack = err
+      .stack!.split('\n')
+      .filter(msg => msg.indexOf('errors.') === -1)
+      .join('\n')
+
+    throw err
+  }
+}
